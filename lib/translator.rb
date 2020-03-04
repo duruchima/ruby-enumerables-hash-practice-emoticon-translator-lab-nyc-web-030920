6f560e5388f34a13c355be07026fa92e31abc80d
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 21:57:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def load_library(file_path)
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   load_library(file_path)
-
+  "get_emoticon".find(emoticon)
 end
 
 def get_english_meaning
