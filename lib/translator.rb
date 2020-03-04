83706620fe4cd572ec97f93a0ed977957a53550f
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 21:22:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 # require modules here
 require "yaml"
-def load_library(./lib/emoticons.yml)
+def load_library
   # code goes here
-  emoticons = YAML.load_file('emoticons.yml')
+  emoticons = YAML.load_file('./lib/emoticons.yml')
 end
 
 def get_japanese_emoticon
