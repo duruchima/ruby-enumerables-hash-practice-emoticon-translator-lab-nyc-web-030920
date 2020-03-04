9b9922145bcbde2e78e9dd71431c3641e6517903
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 22:01:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file_path, emoticon)
    library = load_library(file_path)
    if library["get_emoticon"].include? emoticon
      library["get_emoticon"][emoticon]
-   else p"Sorry, that emoticon was not found."
+   else p"Sorry, that emoticon was not found"
    end
 end
 
