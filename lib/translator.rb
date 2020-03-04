c0db3a9fb184358ec7410d5118ddea62c98a7521
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 21:53:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,14 +3,13 @@ require "yaml"
 def load_library(file_path)
   # code goes here
   library = YAML.load_file(file_path)
-  hash= {"get_meaning" => {}, "get_emoticon" => {}}
-  library.each do |meaning, emoticons|
-    hash["get_meaning"][emoticons][1] = meaning
-    hash["get_emoticon"][emoticons][0] = emoticons[1]
-
+    result = {"get_meaning" => {}, "get_emoticon" => {}}
+    library.each do |meaning, emoticons|
+      result["get_meaning"][emoticons[1]] = meaning
+      result["get_emoticon"][emoticons[0]] = emoticons[1]
+    end
+    result
   end
-  hash
-end
 
 def get_japanese_emoticon
   # code goes here
